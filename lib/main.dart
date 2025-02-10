6941e417163e57199bd8d391c392dd34f8ad6bc9
AbdelrahmanAddel <Aa5282243@gmail.com>
2025-02-10 17:37:56 +0200

    ADD env

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sadio_store/core/app/env_varible.dart';
 import 'package:sadio_store/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EnvVarible.instance.envVaribleInit(variableType: EnvironmentType.dev);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -15,8 +23,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SizedBox(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: EnvVarible.instance.envType,
+      home: Placeholder(),
     );
   }
 }
